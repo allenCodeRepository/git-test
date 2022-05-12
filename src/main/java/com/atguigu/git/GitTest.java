23b8abe651cacc4f94bc5c21ac3c2d8a3e0a7a2c
allen <allen@long.com>
2022-05-12 17:53:08 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ALLEN
+ * @create 2022-05-12 17:51
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
